1753d6e986c8895a906f741758e33a1585d9df2d
mekh <developermekh@gmail.com>
2023-10-29 23:10:08 +0200

    add +1 sonar error

--- src/main/java/Dispatcher.java
+++ src/main/java/Dispatcher.java
@@ -1,4 +1,8 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
+@SpringBootApplication // Noncompliant; RootBootApp is declared in the default package
 public class Dispatcher {
 
     public static void main(String[] args){
