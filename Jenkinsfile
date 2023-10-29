cb2920ea52a0949924a671c7f850110fdff4e973
mekh <developermekh@gmail.com>
2023-10-29 18:47:41 +0200

    add sonar bugs

--- Jenkinsfile
+++ Jenkinsfile
@@ -25,13 +25,7 @@ pipeline {
         stage("sonar") {
 
             steps {
-                sh '''
-                    mvn clean verify sonar:sonar \
-                      -Dsonar.projectKey=mkh2514543_sonar_AYt8AztKnn269OD-fPh2 \
-                      -Dsonar.projectName='sonar' \
-                      -Dsonar.host.url=http://localhost:9000 \
-                      -Dsonar.token=sqp_50e54febc8128148972ff7028ccc7167e57c65f5
-                '''
+                sh 'mvn sonar:sonar'
             }
         }
 
--- src/main/java/service/SonarQubeImpl.java
+++ src/main/java/service/SonarQubeImpl.java
@@ -2,9 +2,27 @@ package service;
 
 import dto.ResponseDto;
 
-public class SonarQubeImpl implements SonarQube{
+public class SonarQubeImpl implements SonarQube {
     @Override
     public ResponseDto testByEnv(String env) {
         return new ResponseDto("VALID");
     }
+
+    private void sonarInvalidCode() {
+        String firstName = getFirstName(); // String overrides equals
+        String lastName = getLastName();
+
+        if (firstName == lastName) {
+            System.out.println("Block for sonar!");
+        }// Non-compliant; false even if the strings have the same value
+    }
+
+    private String getLastName() {
+        return "Last name";
+    }
+
+    private String getFirstName() {
+        return "First name";
+    }
+
 }
